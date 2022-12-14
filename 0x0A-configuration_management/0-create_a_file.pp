b390e8928cb6ab83ce6b64a93ab5a8c36b274cfc
abel-solo <abel.solo2011@yahoo.com>
2022-12-14 11:31:29 -0800

    creating a file in puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# creating a file using puppet unsing below specification
+
+file { '/tmp/school':
+  path    => '/tmp/scchool',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
