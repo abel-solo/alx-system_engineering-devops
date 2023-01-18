f8ebe42af5ffe309a8ec26a1792ac961a3921bc9
abel-solo <abel.solo2011@yahoo.com>
2023-01-18 20:36:56 +0000

    export to JSON

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import json
+import requests
+import sys
+
+if __name__ == '__main__':
+    user_sys = sys.argv[1]
+    url = 'https://jsonplaceholder.typicode.com'
+    usr = requests.get(url + "users/{}".format(user_sys)).json()
+    username = usr.get("username")
+    to_do = requests.get(url+'todos',params={'user_id':user_sys}).json()
+
+    with open("{}.json".format(user_sys), "w") as jsonfile:
+        json.dump({user_sys: [{"task": e.get("title"),
+                              "completed": e.get("completed"),
+                              "username": username} for e in to_do]},
+                  jsonfile)
