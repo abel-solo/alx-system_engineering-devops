3f7abff54b04baa47841bd0373d2780174d0b272
abel-solo <abel.solo2011@yahoo.com>
2023-01-18 21:06:38 +0000

    dictionary of list

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python3
 '''script to export data in the JSON format'''
 import json
+from sys import argv
 import requests
 
 
 if __name__ == '__main__':
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users").json()
+    url = 'https://jsonplaceholder.typicode.com'
+    users = requests.get(url + '/users/').json()
+    to_do = requests.get(url + '/todos/').json()
 
-    with open("todo_all_employees.json", "w") as jsonfile:
-        json.dump({user.get("id"): [{
-            "username": user.get("username"),
-            "task": e.get("title"),
-            "completed": e.get("completed")
-        } for e in requests.get(url + "todos",
-                         params={"userId": usr.get("id")}).json()]
-            for user in user}, jsonfile)
+    i, records = 0, []
+    id, user, all = users[i]['id'], users[i]['username'], {}
+
+    for todo in to_do:
+        if todo['userId'] != id:
+            all[id] = records
+            i += 1
+            id, user = users[i]['id'], users[i]['username']
+            records = []
+        records.append({'username': user, 'task': todo['title'],
+                        'completed': todo['completed']})
+
+    all[id] = records
+    with open('todo_all_employees.json', 'w') as file:
+        json.dump(all, file)
